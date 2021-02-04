d9b3244018e335ac7db753af69b4abda36be76ee
nikolaskorakidis <korakidisn@gmail.com>
2021-02-04 21:40:53 +0100

    part 1

---
+++ pages/posts/first-post.js
@@ -0,0 +1,21 @@
+import Link from "next/link";
+
+export default function FirstPost() {
+  return (
+    <>
+      <h1>First Post</h1>
+      <h2>
+        <Link href="/">
+          <a>Back to home</a>
+        </Link>
+      </h2>
+      <Link href="/">
+        <a className="foo" target="_blank" rel="noopener noreferrer">
+          Hello World
+        </a>
+      </Link>
+    </>
+  );
+}
+
+// https://nextjs.org/learn/basics/assets-metadata-css
