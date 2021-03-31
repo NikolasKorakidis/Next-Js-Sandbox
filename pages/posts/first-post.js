645860ab6e01b128f610e039da39eefba329ab82
nikolaskorakidis <korakidisn@gmail.com>
2021-03-31 16:04:34 +0200

    Assets, Metadata, and CSS

--- pages/posts/first-post.js
+++ pages/posts/first-post.js
@@ -1,21 +1,19 @@
+import Head from "next/head";
 import Link from "next/link";
+import Layout from "../../components/layout";
 
 export default function FirstPost() {
   return (
-    <>
+    <Layout>
+      <Head>
+        <title>First Post</title>
+      </Head>
       <h1>First Post</h1>
       <h2>
         <Link href="/">
           <a>Back to home</a>
         </Link>
       </h2>
-      <Link href="/">
-        <a className="foo" target="_blank" rel="noopener noreferrer">
-          Hello World
-        </a>
-      </Link>
-    </>
+    </Layout>
   );
 }
-
-// https://nextjs.org/learn/basics/assets-metadata-css
